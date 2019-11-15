cf2406f6a606eca6323dc1bc6b2156cff620f667
Andrew Dolgov <noreply@fakecake.org>
2019-11-15 17:42:50 +0300

    various updates

--- app/startup.sh
+++ app/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -ex
 
-OWNER=nobody
 DST_DIR=/var/www/html/tt-rss
 SRC_REPO=https://git.tt-rss.org/fox/tt-rss.git
 
@@ -15,7 +14,13 @@ else
 	cd $DST_DIR && git pull origin master
 fi
 
-chown -R $OWNER $DST_DIR
+if [ ! -d $DST_DIR/plugins.local/nginx_xaccel ]; then
+	git clone https://git.tt-rss.org/fox/ttrss-nginx-xaccel.git $DST_DIR/plugins.local/nginx_xaccel
+else
+	cd $DST_DIR/plugins.local/nginx_xaccel && git pull origin master
+fi
+
+chown -R $OWNER_UID:$OWNER_GID $DST_DIR
 chmod +x /etc/periodic/15min/*
 
 for d in cache lock feed-icons; do
@@ -30,10 +35,11 @@ if [ ! -s $DST_DIR/config.php ]; then
 	SELF_URL_PATH=$(echo $SELF_URL_PATH | sed -e 's/[\/&]/\\&/g')
 
 	sed \
-		-e "s/define('DB_HOST'.*/define('DB_HOST','$DB_HOST');/" \
-		-e "s/define('DB_USER'.*/define('DB_USER','$DB_USER');/" \
-		-e "s/define('DB_NAME'.*/define('DB_NAME','$DB_NAME');/" \
-		-e "s/define('DB_PASS'.*/define('DB_PASS','$DB_PASS');/" \
+		-e "s/define('DB_HOST'.*/define('DB_HOST', '$DB_HOST');/" \
+		-e "s/define('DB_USER'.*/define('DB_USER', '$DB_USER');/" \
+		-e "s/define('DB_NAME'.*/define('DB_NAME', '$DB_NAME');/" \
+		-e "s/define('DB_PASS'.*/define('DB_PASS', '$DB_PASS');/" \
+		-e "s/define('PLUGINS'.*/define('PLUGINS', 'auth_internal, note, nginx_xaccel');/" \
 		-e "s/define('SELF_URL_PATH'.*/define('SELF_URL_PATH','$SELF_URL_PATH');/" \
 		< $DST_DIR/config.php-dist > $DST_DIR/config.php
 fi
